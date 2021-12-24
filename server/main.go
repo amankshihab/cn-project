82603d2bf820dfd3034f63c828a84902affd607e
amankshihab <amanshihab276@gmail.com>
2021-12-25 01:39:50 +0530

    Added delete, cat

--- server/main.go
+++ server/main.go
@@ -18,8 +18,8 @@ func main() {
 
 		os.Exit(1)
 	}
-
 	defer l.Close()
+	fmt.Println("Listening on port 8080..")
 
 	for {
 		c, err := l.Accept()
@@ -45,22 +45,40 @@ func handleConnection(c net.Conn) {
 		bufferLower := strings.ToLower(string(buffer))
 
 		operation := bufferLower[:2]
-		fmt.Println(operation)
 
-		// switch operation {
+		switch operation {
 
-		// 	case "cr":
-		// 		file , err := os.Create(bufferLower[7:])
-		// 		if err != nil {
-		// 			c.Write([]byte("Error while creating file, try again later."))
-		// 		}
-		// 		file.Close()
-		// 		c.Write([]byte("File created"))
+		// Creates a new file
+		case "cr":
+			file, err := os.Create(strings.Trim(bufferLower[7:], " \n"))
+			if err != nil {
+				c.Write([]byte("Error while creating file, try again later."))
+			}
+			file.Close()
+			c.Write([]byte("File created"))
 
-		// 	default: 
-		// 		c.Write([]byte("Command not recognized"))
-		// }
+		// same effect as executing cat command on a file
+		case "ca":
+			content, err := os.ReadFile(strings.Trim(bufferLower[4:], " \n"))
+			if err != nil {
+				c.Write([]byte("Won't work mate"))
+			}
+			if len(content) != 0 {
+				c.Write(content)
+			} else {
+				c.Write([]byte(" "))
+			}
+		
+		// deletes a file
+		case "de":
+			err := os.Remove(strings.Trim(bufferLower[7:], " \n"))
+			if err != nil {
+				c.Write([]byte("Error deleting the files"))
+			}
+			c.Write([]byte("File Deleted"))
 
-		c.Write([]byte("Did it"))
+		default:
+			c.Write([]byte("Command not recognized"))
+		}
 	}
 }
